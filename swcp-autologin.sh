0c9534aba2d108ea08970170b4b056656cd27545
ael-code <tommy.ael@gmail.com>
2015-03-26 17:12:01 +0100

    definitive one

--- swcp-autologin.sh
+++ swcp-autologin.sh
@@ -16,20 +16,26 @@ fi
 #controls if you are connected to the right AP
 essid=`iwgetid -r`
 if [ "$essid" != "$TARGET_ESSID" ]; then
-        log_debug "Not connected to the target AP"
-        exit 0;
+    log_debug "Not connected to the target AP"
+    exit 0;
 fi
 
 #controls if you are already logged
 http_code=`curl --max-time $PROBE_TIMEOUT -s -o /dev/null -I -w "%{http_code}" http://clients3.google.com/generate_204`
 if [ $http_code == 204 ]; then
-        log_debug "Already logged in"
-        exit 0;
+    log_debug "Already logged in"
+    exit 0;
 fi
 
 # actual request
-curl -s \
+curl -s -o /dev/null \
+--retry 10 \
 -d "auth_user=$CPAL_USER" \
 -d "auth_pass=$CPAL_PASS" \
 -d "accept=Continue" \
 $PORTAL_URL
+
+if [ $? -eq 0 ]; then
+    log_debug "Login request successful sended"
+    exit 0;
+fi
